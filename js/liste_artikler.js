b2407000b284f9842cc601f2d2710b677e54c207
Heiberik <37657797+Heiberik@users.noreply.github.com>
2019-12-28 16:58:16 +0100

    lagt til google analytics script

--- js/liste_artikler.js
+++ js/liste_artikler.js
@@ -1,9 +1,25 @@
-let liste = document.getElementById('liste_artikler');
+var artikler = []
+var liste_artikler = document.getElementById("liste_artikler");
+
+artikler.push("../html/artikler/artikkel1.html");
+artikler.push("../html/artikler/artikkel2.html");
+artikler.push("../html/artikler/artikkel3.html");
+artikler.push("../html/artikler/artikkel4.html");
 
 function visListe(){
 
-    liste.
 
+    for (indeks = 0; indeks < artikler.length; indeks++) {
+
+        var nyLi = document.createElement("li");
+        var html = artikler[indeks];
+
+        var content = document.createTextNode(html)
+
+        nyLi.appendChild(content);
+
+        liste_artikler.appendChild(nyLi);
+    }
 }
 
 

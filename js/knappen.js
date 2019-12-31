eb50c74d19f9cf53ee50a57e490c82447c4ba98c
Heiberik <37657797+Heiberik@users.noreply.github.com>
2019-12-31 13:15:00 +0100

    ordnet knappen!

--- js/knappen.js
+++ js/knappen.js
@@ -1,48 +1,95 @@
 const evilButton = document.getElementById('evil-button')
-const OFFSET = 100
+const OFFSET = 50
+var teller = 0
+const tekst = ['STOPP DA','HARASSMENT','IKKEEEE!', 'IKKE KLIKK MEG', '']
+
+
+$(window).scroll(function() {
+
+    if ($(this).scrollTop()>0)
+     {
+        $('#evil-button').fadeOut();
+     }
+    else
+     {
+      $('#evil-button').fadeIn();
+     }
+ });
 
 evilButton.addEventListener('click', () => {
-  alert('Nice Try')
-  window.close()
+    alert('HEHEHE NEI')
+    window.location.href = '../index.html';
 })
 
+
 document.addEventListener('mousemove', (e) => {
-  const x = e.pageX
-  const y = e.pageY
-  const buttonBox = evilButton.getBoundingClientRect()
-  const horizontalDistanceFrom = distanceFromCenter(buttonBox.x, x, buttonBox.width)
-  const verticalDistanceFrom = distanceFromCenter(buttonBox.y, y, buttonBox.height)
-  const horizontalOffset = buttonBox.width / 2 + OFFSET
-  const verticalOffset = buttonBox.height / 2 + OFFSET
-  if (Math.abs(horizontalDistanceFrom) <= horizontalOffset && Math.abs(verticalDistanceFrom) <= verticalOffset) {
-    setButtonPosition(
-      buttonBox.x + horizontalOffset / horizontalDistanceFrom * 10,
-      buttonBox.y + verticalOffset / verticalDistanceFrom * 10
-    )
-  }
+
+
+    const x = e.pageX
+    const y = e.pageY
+    const buttonBox = evilButton.getBoundingClientRect()
+
+    const horizontalDistanceFrom = distanceFromCenter(buttonBox.x, x, buttonBox.width)
+    const verticalDistanceFrom = distanceFromCenter(buttonBox.y, y, buttonBox.height)
+
+    const horizontalOffset = buttonBox.width / 2 + OFFSET
+    const verticalOffset = buttonBox.height / 2 + OFFSET
+
+    if (Math.abs(horizontalDistanceFrom) <= horizontalOffset &&
+    Math.abs(verticalDistanceFrom) <= verticalOffset) {
+        setButtonPosition(
+            buttonBox.x + horizontalOffset / horizontalDistanceFrom * 5,
+            buttonBox.y + verticalOffset / verticalDistanceFrom * 5
+        )
+    }
+})
+
+document.addEventListener('scroll', (e) => {
+    const x = e.pageX
+    const y = e.pageY
+    const buttonBox = evilButton.getBoundingClientRect()
+
+    const horizontalDistanceFrom = distanceFromCenter(buttonBox.x, x, buttonBox.width)
+    const verticalDistanceFrom = distanceFromCenter(buttonBox.y, y, buttonBox.height)
+
+    const horizontalOffset = buttonBox.width / 2 + OFFSET
+    const verticalOffset = buttonBox.height / 2 + OFFSET
+
+    if (Math.abs(horizontalDistanceFrom) <= horizontalOffset &&
+    Math.abs(verticalDistanceFrom) <= verticalOffset) {
+        setButtonPosition(
+            buttonBox.x + horizontalOffset / horizontalDistanceFrom * 5,
+            buttonBox.y + verticalOffset / verticalDistanceFrom * 5
+        )
+    }
 })
 
 function setButtonPosition(left, top) {
-  const windowBox = document.body.getBoundingClientRect()
-  const buttonBox = evilButton.getBoundingClientRect()
+    teller++;
+    if (teller % 50 == 0) {
+        evilButton.innerHTML = tekst[Math.floor(Math.random() * tekst.length)];
+    }
 
-  if(distanceFromCenter(left, windowBox.left, buttonBox.width) < 0) {
-    left = windowBox.right - buttonBox.width - OFFSET
-  }
-  if(distanceFromCenter(left, windowBox.right, buttonBox.width) > 0) {
-    left = windowBox.left + OFFSET
-  }
-  if(distanceFromCenter(top, windowBox.top, buttonBox.height) < 0) {
-    top = windowBox.bottom - buttonBox.height - OFFSET
-  }
-  if(distanceFromCenter(top, windowBox.bottom, buttonBox.height) > 0) {
-    top = windowBox.top + OFFSET
-  }
+    const windowBox = document.getElementById('body_knappen').getBoundingClientRect()
+    const buttonBox = evilButton.getBoundingClientRect()
 
-  evilButton.style.left = `${left}px`
-  evilButton.style.top = `${top}px`
+    if (distanceFromCenter(left, windowBox.left, buttonBox.width) < 0) {
+        left = windowBox.right - buttonBox.width - OFFSET
+    }
+    if (distanceFromCenter(left, windowBox.right, buttonBox.width) > 0) {
+        left = windowBox.left + OFFSET
+    }
+    if (distanceFromCenter(top, windowBox.top, buttonBox.height) < 0) {
+        top = windowBox.bottom - buttonBox.height - OFFSET
+    }
+    if (distanceFromCenter(top, windowBox.bottom, buttonBox.height) > 0) {
+        top = windowBox.top + OFFSET
+    }
+
+    evilButton.style.left = `${left}px`
+    evilButton.style.top = `${top}px`
 }
 
 function distanceFromCenter(boxPosition, mousePosition, boxSize) {
-  return boxPosition - mousePosition + boxSize / 2
+    return boxPosition - mousePosition + boxSize / 2
 }
